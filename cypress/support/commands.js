ecbaf7744e766e6e30bce01f7702908f07d625fd
Nikita Babichenko <babichenkona@gmail.com>
2022-11-10 10:02:32 +0300

    wip

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,20 +1,3 @@
-const { MailSlurp } = require('mailslurp-client');
-
-// set your api key with an environment variable `CYPRESS_API_KEY` or configure using `env` property in config file
-// (cypress prefixes environment variables with CYPRESS)
-const apiKey = Cypress.env('API_KEY')
-const mailslurp = new MailSlurp({ apiKey });
-
-Cypress.Commands.add("createInbox", () => {
-    return mailslurp.createInbox();
-});
-
-Cypress.Commands.add("waitForLatestEmail", (inboxId) => {
-    // how long we should hold connection waiting for an email to arrive
-    const timeoutMillis = 30_000;
-    return mailslurp.waitForLatestEmail(inboxId, timeoutMillis)
-});
-
 Cypress.Commands.add('login', (email, password) => {
     cy.visit('https://itdelta.learn.company-policy.com/login', { timeout: 10000 });
 
