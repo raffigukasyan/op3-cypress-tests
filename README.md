f563fab149190a9a0c9cc96253ad11b69036fb25
Maxim Gerzon <gerzon@it-delta.ru>
2022-11-20 17:36:39 +0300

    password removed to as-a

--- README.md
+++ README.md
@@ -1,15 +1,24 @@
 # op3-cypress-tests
 
 # Requirenments
-Java 8+
 
-Insallet [allure](https://docs.qameta.io/allure/#_installing_a_commandline)
+Node.js
+
+Installed [allure](https://docs.qameta.io/allure/#_installing_a_commandline)
+
+Installed [as-a](https://github.com/bahmutov/as-a)
+
+Created ~/.as-a.ini
+```
+[op3]
+CYPRESS_password=secret
+```
 
 # Getting started
 
 npm install 
 
-npx cypress run --env allure=true
+npx as-a op3 npx cypress run --env allure=true
 
 npm run allure:report
 
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -8,10 +8,16 @@ describe('A. Login test', () => {
     });
 
     it('should move to login page and log in', function () {
+        const username = Cypress.env('email')
+        const password = Cypress.env('password')
+
+        expect(username, 'username was set').to.be.a('string').and.not.be.empty
+        expect(password, 'password was set').to.be.a('string').and.not.be.empty
+
         cy.xpath("//a[@class='whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900']").click();
 
-        cy.xpath("//input[@id='email']", { timeout: 10000 }).type(Cypress.env('email'));
-        cy.xpath("//input[@id='password']", { timeout: 10000 }).type(Cypress.env('password'), { log: false });
+        cy.xpath("//input[@id='email']", { timeout: 10000 }).type(username);
+        cy.xpath("//input[@id='password']", { timeout: 10000 }).type(password, { log: false });
 
         cy.xpath("//button[@type='submit']", { timeout: 10000}).click();
 
