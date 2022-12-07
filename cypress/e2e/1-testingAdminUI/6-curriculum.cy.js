050177d5be9e0832034a3bc0b966cac829b174e0
Maxim Gerzon <gerzon@it-delta.ru>
2022-12-07 14:33:34 +0300

    curriculum fixed

--- cypress/e2e/1-testingAdminUI/6-curriculum.cy.js
+++ cypress/e2e/1-testingAdminUI/6-curriculum.cy.js
@@ -28,15 +28,24 @@ describe('H. Create curriculum', () => {
         cy.xpath("//button[text()='Add curriculum']").click();
 
         cy.xpath("(//input[@type='text'])[1]").type(Cypress.env('curriculumName'));
-        cy.xpath("//textarea").type("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci " +
-            "eligendi harum hic quidem. Aliquam amet architecto, id illum laboriosam maxime nobis omnis perspiciatis " +
-            "porro provident, quidem reiciendis sequi voluptate voluptatem.")
+        cy.xpath("//textarea").type("Lorem ipsum dolor sit amet, consectetur adipisicing elit.")
 
         cy.xpath("(//input[@type='text'])[2]").type(Cypress.env('courseName'));
-        cy.xpath("//*[text()='" + Cypress.env('courseName') + "']").click();
+        cy.xpath("//*[text()='" + Cypress.env('courseName') + "'][1]").click();
 
         // Save curriculum
         cy.xpath("//button[text()='Save']").click();
+
+        // Assert curriculum created
+        cy.xpath("//p[text()='Success!']", { timeout: 5000 }).should('be.visible');
+    });
+
+    it('should delete curriculum', function () {
+        // Go to add curriculums page
+        cy.xpath("//a[text()='Curriculums']").click();
+
+        // Save curriculum
+        cy.xpath("//div[text()='QA Test Curriculum']/../../../../following-sibling::th[3]/div/div[2]").last().click();
 
         // Assert curriculum created
         cy.xpath("//p[text()='Success!']", { timeout: 5000 }).should('be.visible');
