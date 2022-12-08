1a4fce33026ecadefd0b00ef79c78062be953d7a
Maxim Gerzon <gerzon@it-delta.ru>
2022-12-08 21:18:45 +0300

    fixed Categories test

--- cypress/e2e/1-testingAdminUI/1-testingAdminCP/1-Categories.cy.js
+++ cypress/e2e/1-testingAdminUI/1-testingAdminCP/1-Categories.cy.js
@@ -7,13 +7,13 @@ describe("A. Categories List", () => {
     });
 
     beforeEach(() => {
-        
+        Cypress.Cookies.preserveOnce('company_policy_session');
     });
 
     it('should create Category)', function () {
         cy.visit('admin/cp/category');
         cy.contains('Categories').click();
-        cy.contains('addCategory').click();
+        cy.contains('Add category').click();
 
         // create post
         cy.get('ul li:first input').type(catName);
@@ -23,24 +23,24 @@ describe("A. Categories List", () => {
         cy.xpath("//p[text()='Success!']").should('be.visible');
 
         // check active 
-        // cy.xpath(`//div[text()='Test category 1']/../../../../th[2]`).first().should('have.value', 'Inactive');
-        cy.xpath(`//div[text()='Test category 1']/../../../../../th[2]`).first().contains('Inactive');
-        // cy.xpath(`//div[text()='${catName}']/../../../../following-sibling::th[1]`).contains(catName).first().should('have.value', 'Inactive');
-        //// Edit lesson ////
-        // Getting access to lesson
-        // cy.accessAllItems();
-        // cy.xpath("(//div[text()='" + Cypress.env('lessonCheckboxRadio') + "'])[1]").click();
+        cy.xpath(`//div[text()='${catName}']/../../../../../th[2]`).last().contains('Inactive');
+    });
 
-        // // Create radio question
-        // cy.question(Cypress.env('questionRadio'), 2);
-        // cy.addAnswers(1);
+    it('should edit Category)', function () {
+        cy.visit('admin/cp/category');
 
-        // // Create checkbox question
-        // cy.question(Cypress.env('questionCheckbox'), 3);
-        // cy.addAnswers(2);
+        cy.accessAllItems();
+        cy.xpath(`(//div[text()='${catName}'])`).last().click();
 
-        // // Assert question added
-        // cy.xpath("//span[text()='Active']").should('be.visible');
+        cy.contains('Edit category');
+        
+        cy.xpath("//button[@role='switch']").click();
+
+        cy.xpath("//button[text()='Save']").should('be.visible').click();
+        cy.wait(1000);
+        cy.xpath("//p[text()='Success!']").should('be.visible');
+
+        cy.xpath(`//div[text()='${catName}']/../../../../../th[2]`).last().contains('Active');
     });
 
     it('should delete Category)', function () {
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -54,7 +54,7 @@ Cypress.Commands.add('addAnswers', (answer) => {
 });
 
 Cypress.Commands.add('accessAllItems', () => {
-    // cy.wait(3000);
+    cy.wait(1000);
     cy.xpath("(//button[@class='relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-pointer focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 text-sm'])[last()]")
     .click();
     cy.wait(1000);
