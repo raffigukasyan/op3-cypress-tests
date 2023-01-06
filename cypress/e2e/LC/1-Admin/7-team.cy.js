35c52c8717e68c60bfa6e458614c146b953037d6
Maxim Gerzon <gerzon@it-delta.ru>
2023-01-06 16:42:08 +0300

    team deleting fixed

--- cypress/e2e/LC/1-Admin/7-team.cy.js
+++ cypress/e2e/LC/1-Admin/7-team.cy.js
@@ -12,20 +12,13 @@ describe("LC.A5. Create team", () => {
 
         // Input credentials
         cy.xpath("(//input[@type='text'])[1]").type(tName);
-        cy.xpath("//textarea").type("Lorem ipsum dolor sit amet, consectetur adipisicing elit. " +
-            "Cupiditate magnam numquam porro praesentium temporibus totam.");
+        cy.xpath("//textarea").type("Lorem ipsum dolor sit amet, consectetur adipisicing elit.");
 
         cy.xpath("//button[text()='Save']").click();
 
         // Assert team created
         cy.xpath("//p[text()='Success!']", { timeout: 5000 }).should('be.visible');
 
-        // Delete team
-        cy.xpath("(//*[@class='w-5 h-5 mx-1 text-red-600 hover:text-red-900 cursor-pointer'])[last()]").click();
-        cy.get('button').contains('Delete').click();
-
-        // Assert team deleted
-        cy.xpath("//p[text()='Success!']", { timeout: 5000 }).should('be.visible');
     });
 
     it('should delete team', function () {
