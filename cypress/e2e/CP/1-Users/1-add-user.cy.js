b979b6f859e3dc39ffbfd0073c8ceeacc8074be7
itdelta-prog <delta@it-delta.ru>
2023-12-20 15:32:49 +0300

    add test User Company Policy

---
+++ cypress/e2e/CP/1-Users/1-add-user.cy.js
@@ -0,0 +1,70 @@
+describe("US.1 Add User", () => {
+
+    let email = `qaUserManual${Math.random()}@mail.ru`;
+    let password = `Qa${Math.random()}`
+    beforeEach(() => {
+        cy.admin();
+        cy.xpath("//a[text()='Пользователи']").click();
+    })
+
+    it('Add user', function () {
+        // Go to add user page
+        cy.xpath("//button[text()='Добавить пользователя']").click();
+
+        cy.wait(1000);
+
+        // filling in the fields
+        cy.xpath("//span[text()='Имя *']").next().type('QA');
+        cy.xpath("//span[text()='Фамилия']").next().type('USER')
+        cy.xpath("//span[text()='Почта *']").next().type(email);
+        cy.xpath("//span[text()='Телефон']").next().type('+7 999 999 99 99');
+        cy.xpath("//span[text()='Пароль *']").next().type(password);
+        cy.xpath("//span[text()='Повторите пароль *']").next().type(password);
+        cy.xpath("//span[text()='Активность']").next().children().click();
+
+        // Add IMAGE
+        cy.xpath('//input[@id="avatar"]').selectFile('cypress/image/qaUser.jpg', {force: true});
+
+        cy.xpath("//button[text()='Выбрать']").click();
+        cy.wait(500);
+        cy.contains("Выбрать: Отделы").parent().next().click();
+        cy.wait(500);
+        cy.xpath("//div[text()='Маркетинг']").click();
+        cy.contains("Выбрать: Отделы").parent().parent().last().click();
+        cy.wait(500);
+
+        //Save
+        cy.xpath("//button[text()='Сохранить']").click();
+        cy.wait(2000);
+        cy.contains('Пользователь успешно обновлён!').should('be.visible');
+    })
+
+    it('check add User', () => {
+        cy.contains('qaUserManual0.492022085662037@mail.ru').click()
+    })
+
+    it('edite User', () => {
+        cy.contains('qaUserManual0.492022085662037@mail.ru').parent().parent().last().find('div').first().click();
+        cy.wait(1500);
+
+        // filling in the fields
+        cy.xpath("//span[text()='Имя *']").next().clear().type('QA QA');
+        cy.xpath("//span[text()='Фамилия']").next().clear().type('USER USER');
+
+        email = 'Edit' + email;
+        password = password + 'Edit';
+
+        cy.xpath("//span[text()='Почта *']").next().clear().type(email);
+        cy.xpath("//span[text()='Телефон']").next().clear().type('+7 999 999 99 99');
+        cy.xpath("//span[text()='Пароль *']").next().clear().type(password+'Edit');
+        cy.xpath("//span[text()='Повторите пароль *']").next().clear().type(password+"Edit");
+        cy.xpath('//input[@id="avatar"]').selectFile('cypress/image/editQaUser.avif', {force: true});
+        cy.xpath("//span[text()='Активность']").next().children().click();
+
+        //Save
+        cy.xpath("//button[text()='Сохранить']").click();
+        cy.wait(2000);
+        cy.contains('Пользователь успешно обновлён!').should('be.visible');
+    })
+
+})
