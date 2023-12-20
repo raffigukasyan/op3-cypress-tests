a464886aac2f32b4db33c260a00fd1a72fcdca71
Padatoo <46717709+Padatoo@users.noreply.github.com>
2023-12-20 14:48:36 +0300

    init commit

---
+++ cypress/e2e/LC/Landing-tests/4-Land-RU-contact-sales.cy.js
@@ -0,0 +1,17 @@
+Подробнееdescribe('1. Auth-login-page', () => {
+    beforeEach(() => {
+        cy.visit(Cypress.config().authUrl);
+    });
+
+    it('should move to login page', function () {
+        cy.wait(1000);
+        cy.xpath("//input[@id='email']", { timeout: 10000 }).should('be.visible');
+
+        cy.xpath("//input[@id='password']", { timeout: 10000 }).should('be.visible');
+
+        cy.xpath("//button[@type='submit']", { timeout: 10000}).should('be.visible');
+
+        cy.xpath("//h2[text()='Войти']").should('be.visible');
+
+    });
+})
