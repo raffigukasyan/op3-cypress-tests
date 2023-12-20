107c465e5f62beb1a51354db8a7879badf8cb313
Padatoo <46717709+Padatoo@users.noreply.github.com>
2023-12-20 18:32:39 +0300

    landing #5 with modal click for RU

--- cypress/e2e/LC/Landing-tests/4-Land-RU-contact-sales.cy.js
+++ cypress/e2e/LC/Landing-tests/4-Land-RU-contact-sales.cy.js
@@ -5,8 +5,9 @@ describe('4. Land-RU-contact-sales', () => {
 
     it('should move to login page', function () {
         cy.wait(2000);
-        //cy.get(" →").eq(0).should('be.visible').click();
+        //cy.get(" →").eq(0).should('be.visible').click();'[id="headlessui-menu-item-:r4:"]'
         cy.wait(1000);
-        cy.contains(" →");
+        cy.get('[class="text-base font-medium text-blue-700 hover:text-blue-600"]').contains("Связаться").click();
+
     });
 })
