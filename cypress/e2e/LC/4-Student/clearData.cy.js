0746e20642bc3932b0a3d3281631d034010f150c
itdelta-prog <delta@it-delta.ru>
2024-01-12 14:45:12 +0300

    fix test

--- cypress/e2e/LC/4-Student/clearData.cy.js
+++ cypress/e2e/LC/4-Student/clearData.cy.js
@@ -24,14 +24,14 @@ describe('LC.Z. Clear all created learning items', () => {
 
     it('should delete lessons', function () {
         cy.visit('/admin/lc/lessons');
-        cy.wait(500);
+        cy.wait(1500);
         cy.accessAllItems();
         cy.xpath(`//div[text()='${Cypress.env('lessonText')}']`).parent().parent().parent().parent().parent().find('.tooltip').last().click();
         cy.get('button').contains('Delete').click();
         cy.wait(500);
         cy.xpath("//p[text()='Success!']").should('be.visible');
 
-        cy.wait(500);
+        cy.wait(1000);
         cy.visit('/admin/lc/lessons');
         cy.wait(1500);
         cy.xpath(`//div[text()='${Cypress.env('lessonCheckboxRadio')}']`).parent().parent().parent().parent().parent().find('.tooltip').last().click();
