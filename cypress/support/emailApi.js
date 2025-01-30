ba7856d57d32187fadcef50f92b6e52213cd7681
Raffi <raffi.gukasyan777@gmail.com>
2025-01-30 12:20:09 +0300

    edits

--- cypress/support/emailApi.js
+++ cypress/support/emailApi.js
@@ -1,36 +1,38 @@
 const { MailSlurp } = require('mailslurp-client');
 
-const emailApi = async() => {
-    try  {
-        const mailslurp = new MailSlurp({ apiKey: "6d7ea33a0265e867786b4b744db25c3f677cab6516818650caefb2517649312c" });
-        const inbox = await mailslurp.inboxController.createInboxWithDefaults();
-        console.log('Временный email:',inbox);
 
-        const getEmailAccount = () => {
-            return inbox.emailAddress;
+const emailApi = async () => {
+    try {
+        const mailslurp = new MailSlurp({
+            apiKey: "6d7ea33a0265e867786b4b744db25c3f677cab6516818650caefb2517649312c",
+            basePath: "https://api.mailslurp.com"
+        });
+
+        const inboxes = await mailslurp.getInboxes();
+        for (const inbox of inboxes) {
+            console.log(`Удаление почтового ящика: ${inbox.id}`);
+            await mailslurp.deleteInbox(inbox.id);
         }
 
+        const inbox = await mailslurp.createInbox();
+        console.log('Временный email:', inbox.emailAddress);
+
+        const getEmailAccount = () => inbox.emailAddress;
 
         const getEmailData = async () => {
-            //2. Ждём письмо (таймаут 30 секунд)
             console.log('Ожидание письма...');
-            const email = await mailslurp.waitForLatestEmail(inbox.id, 30000);
+            try {
+                const email = await mailslurp.waitForLatestEmail(inbox.id, 30000);
+                return { html: email.body };
+            } catch (error) {
+                console.log('Ошибка ожидания письма:', error);
+            }
+        };
 
-            return {
-                html:email.body
-            };
-
-        }
-
-        return {
-            getEmailAccount,
-            getEmailData
-        }
+        return { getEmailAccount, getEmailData };
+    } catch (error) {
+        console.log("ERROR", error);
     }
-    catch (e) {
-        console.log("ERROR");
-    }
+};
 
-}
-
-module.exports = emailApi
+module.exports = emailApi;
