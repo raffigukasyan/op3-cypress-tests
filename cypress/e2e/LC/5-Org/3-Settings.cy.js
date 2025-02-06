e2eceba20a52a0e564b050270bb52f8221a72706
Raffi <raffi.gukasyan777@gmail.com>
2025-02-06 09:43:47 +0300

    wip edits

--- cypress/e2e/LC/5-Org/3-Settings.cy.js
+++ cypress/e2e/LC/5-Org/3-Settings.cy.js
@@ -17,7 +17,7 @@ describe('OrgBoard.A4.Settings', () => {
     it('edit settings', function () {
 
         cy.xpath("//span[text()='Company name']").next().clear().type('QA COMPANY');
-        cy.xpath("//span[text()='Valuable End Product']").next().clear().type('QA COMPANY DESCRIPTION');
+        cy.xpath("//span[text()='VFP']").next().clear().type('QA COMPANY DESCRIPTION');
 
         cy.xpath("//span[text()='Owners']").next().click();
         cy.xpath("//span[text()='Owners']").next().type('QA position');
@@ -35,7 +35,7 @@ describe('OrgBoard.A4.Settings', () => {
 
     it('defalut settings', function () {
         cy.xpath("//span[text()='Company name']").next().clear().type('Производственная Компания №1');
-        cy.xpath("//span[text()='Valuable End Product']").next().clear().type('Изделия из металла высокого качества, используемые широким кругом профессионалов при строительстве сооружений по всей России');
+        cy.xpath("//span[text()='VFP']").next().clear().type('Изделия из металла высокого качества, используемые широким кругом профессионалов при строительстве сооружений по всей России');
         cy.contains('QA position: User Qa').next().click();
         cy.xpath("//button[text()='Save']").click();
         cy.wait(500);
